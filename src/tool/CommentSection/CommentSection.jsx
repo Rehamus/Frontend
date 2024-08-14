bb962c3552e74d1cd915ed8b59b756c1e2c0f256
Han <nuher038@gmail.com>
2024-08-14 21:03:57 +0900

    8-14

--- src/tool/CommentSection/CommentSection.jsx
+++ src/tool/CommentSection/CommentSection.jsx
@@ -1,11 +1,16 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axiosInstance from '../../api/axiosInstance';
 import './CommentSection.css';
+import { Link } from "react-router-dom";
 
 const CommentSection = ({ postId, isLoggedIn, currentUserId }) => {
     const [comments, setComments] = useState([]);
     const [commentContent, setCommentContent] = useState('');
+    const [replyContents, setReplyContents] = useState({});
+    const [activeReplyId, setActiveReplyId] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [editingCommentId, setEditingCommentId] = useState(null);
+    const [editingContent, setEditingContent] = useState('');
     const [totalPages, setTotalPages] = useState(1);
     const [currentPage, setCurrentPage] = useState(1);
     const [pageSize] = useState(3); // 댓글 페이지당 개수
@@ -68,11 +73,11 @@ const CommentSection = ({ postId, isLoggedIn, currentUserId }) => {
         };
 
         loadLastPage();
-    }, [postId, isLoggedIn, pageSize, fetchComments]); // 의존성 배열에 fetchComments, pageSize 추가
+    }, [postId, isLoggedIn]);
 
     useEffect(() => {
         fetchComments(currentPage - 1);
-    }, [currentPage, fetchComments, pageSize]); // 의존성 배열에 fetchComments, pageSize 추가
+    }, [currentPage]);
 
     // 댓글 작성 처리 함수
     const handleCommentSubmit = async (e) => {
@@ -84,8 +89,89 @@ const CommentSection = ({ postId, isLoggedIn, currentUserId }) => {
             }, {
                 headers: { Authorization: `${localStorage.getItem('Authorization')}` }
             });
-            setCommentContent('');  // 작성 후 입력 필드 비우기
-            fetchComments(totalPages - 1);  // 마지막 페이지 댓글 다시 불러오기
+            setCurrentPage(totalPages); // 마지막 페이지로 이동
+            fetchComments(totalPages - 1);
+            setCommentContent('');
+        } catch (error) {
+            console.error("댓글을 작성하는 중 오류가 발생했습니다!", error);
+        }
+    };
+
+    const handleReplySubmit = async (e, commentId, parentCommentId = null) => {
+        e.preventDefault();
+        try {
+            await axiosInstance.post(`/api/post/${postId}/comments`, {
+                contents: replyContents[commentId] || '',
+                parentId: commentId
+            }, {
+                headers: { Authorization: `${localStorage.getItem('Authorization')}` }
+            });
+            fetchComments(currentPage - 1);  // 현재 페이지에서 댓글 목록 다시 불러오기
+            setReplyContents({ ...replyContents, [commentId]: '' });
+            setActiveReplyId(null);
+        } catch (error) {
+            console.error("대댓글을 작성하는 중 오류가 발생했습니다!", error);
+        }
+    };
+
+    const handleReplyClick = (commentId) => {
+        const newActiveReplyId = activeReplyId === commentId ? null : commentId;
+        setActiveReplyId(newActiveReplyId);
+    };
+
+    const handleReplyContentChange = (e, commentId) => {
+        setReplyContents({ ...replyContents, [commentId]: e.target.value });
+    };
+
+    const handleEditClick = (commentId, content) => {
+        if (editingCommentId === commentId) {
+            setEditingCommentId(null);
+            setEditingContent('');
+        } else {
+            setEditingCommentId(commentId);
+            setEditingContent(content);
+        }
+    };
+
+    const handleEditSubmit = async (e, commentId) => {
+        e.preventDefault();
+        try {
+            await axiosInstance.put(`/api/post/${postId}/comments/${commentId}`, {
+                contents: editingContent
+            }, {
+                headers: { Authorization: `${localStorage.getItem('Authorization')}` }
+            });
+            fetchComments(currentPage - 1); // 현재 페이지에서 댓글 목록 다시 불러오기
+            setEditingCommentId(null);
+            setEditingContent('');
+        } catch (error) {
+            console.error("댓글을 수정하는 중 오류가 발생했습니다!", error);
+        }
+    };
+
+    const handleDeleteClick = async (commentId) => {
+        try {
+            await axiosInstance.delete(`/api/post/${postId}/comments/${commentId}`, {
+                headers: { Authorization: `${localStorage.getItem('Authorization')}` }
+            });
+            fetchComments(currentPage - 1); // 현재 페이지에서 댓글 목록 다시 불러오기
+        } catch (error) {
+            console.error("댓글을 삭제하는 중 오류가 발생했습니다!", error);
+        }
+    };
+
+    const handleLike = async (commentId, liked) => {
+        try {
+            if (liked) {
+                await axiosInstance.delete(`/api/post/${postId}/comments/${commentId}/like`, {
+                    headers: { Authorization: `${localStorage.getItem('Authorization')}` }
+                });
+            } else {
+                await axiosInstance.post(`/api/post/${postId}/comments/${commentId}/like`, {}, {
+                    headers: { Authorization: `${localStorage.getItem('Authorization')}` }
+                });
+            }
+            fetchComments(currentPage - 1); // 현재 페이지에서 댓글 목록 다시 불러오기
         } catch (error) {
             console.error("댓글 작성 중 오류가 발생했습니다!", error);
         }
@@ -96,14 +182,81 @@ const CommentSection = ({ postId, isLoggedIn, currentUserId }) => {
         setCurrentPage(pageNumber);
     };
 
-    // 댓글을 렌더링하는 함수
-    const renderComments = (comments) => {
-        return comments.map((comment) => (
-            <div key={comment.id} className="comment">
-                <p>{comment.contents}</p>
-                {comment.children && renderComments(comment.children)}
-            </div>
-        ));
+    const renderComments = (comments, parentCommentId = null) => {
+        return comments.map(comment => {
+            const isCurrentUser = comment.userId === parseInt(currentUserId, 10);
+            return (
+                <div className={`comment ${isCurrentUser ? 'current-user' : ''}`} key={comment.id}>
+                    <p className="comment-username">
+                        <Link to={`/user/${comment.userId}`}>{comment.nickname}</Link>
+                    </p>
+                    {editingCommentId === comment.id ? (
+                        <form className="update_comment" onSubmit={(e) => handleEditSubmit(e, comment.id)}>
+                            <textarea
+                                value={editingContent}
+                                onChange={(e) => setEditingContent(e.target.value)}
+                                className="edit-textarea"
+                            ></textarea>
+                            <button type="submit" className="button left_button">수정 완료</button>
+                        </form>
+                    ) : (
+                        <p className="comment-content">{comment.contents}</p>
+                    )}
+                    <p className="comment-meta"> {comment.createdAt} 좋아요 {comment.likes}</p>
+                    {isLoggedIn && (
+                        <div className="buttons">
+                            <button
+                                className="reply-button"
+                                onClick={() => handleReplyClick(comment.id)}
+                            >
+                                답글
+                            </button>
+                            {isCurrentUser ? (
+                                <>
+                                    <button
+                                        className="edit-button"
+                                        onClick={() => handleEditClick(comment.id, comment.contents)}
+                                    >
+                                        {editingCommentId === comment.id ? '수정 취소' : '수정'}
+                                    </button>
+                                    <button
+                                        className="delete-button"
+                                        onClick={() => handleDeleteClick(comment.id)}
+                                    >
+                                        삭제
+                                    </button>
+                                </>
+                            ) : (
+                                <button
+                                    className={`reply-button ${comment.likedByUser ? 'liked' : ''}`}
+                                    onClick={() => handleLike(comment.id, comment.likedByUser)}
+                                >
+                                    {comment.likedByUser ? '좋아요 취소' : '좋아요'}
+                                </button>
+                            )}
+                        </div>
+                    )}
+                    {activeReplyId === comment.id && (
+                        <form className="reply-form"
+                              onSubmit={(e) => handleReplySubmit(e, comment.id, parentCommentId)}>
+                            <textarea
+                                name="reply"
+                                placeholder="답글을 입력하세요..."
+                                value={replyContents[comment.id] || ''}
+                                onChange={(e) => handleReplyContentChange(e, comment.id)}
+                                className="reply-textarea"
+                            ></textarea>
+                            <button type="submit" className="button left_button">답글 작성</button>
+                        </form>
+                    )}
+                    {comment.children && comment.children.length > 0 && (
+                        <div className="replies">
+                            {renderComments(comment.children, comment.id)}
+                        </div>
+                    )}
+                </div>
+            );
+        });
     };
 
     if (loading) {
